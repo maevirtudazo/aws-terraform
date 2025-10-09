dc16bfd00e52470a9817e81430837d3321ac9fad
maevirtudazo <maevirtudazo6868@gmail.com>
2025-10-09 16:09:59 +0800

    update main branch

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -2,3 +2,8 @@ variable "environment" {
     description = "Environment name"
     type        = string
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
