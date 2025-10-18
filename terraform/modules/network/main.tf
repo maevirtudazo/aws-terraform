ccbd6bb94a666b5a32c12de7d5cfa7626f91dfd1
maevirtudazo <maevirtudazo6868@gmail.com>
2025-10-18 16:31:55 +0800

    add compute module

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -27,24 +27,24 @@ resource "aws_security_group" "this" {
 
   ingress {
     description = "SSH"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description "HTTP"
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
