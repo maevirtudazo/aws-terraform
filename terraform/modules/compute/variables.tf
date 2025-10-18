1e81a551f404638e2e15769f5be4fb9787e73ff6
maevirtudazo <maevirtudazo6868@gmail.com>
2025-10-18 16:33:55 +0800

    change instance type

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -5,7 +5,7 @@ variable "instance_name" {
 
 variable "instance_type" {
     description = "Type of EC2 instance (e.g., t2.micro)"
-    default     = t3.micro
+    default     = "t3.micro"
 }
 
 variable "ami_id" {
