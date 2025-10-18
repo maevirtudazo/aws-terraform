a39e10e99693d4e6759739bf58a571fc443986e1
maevirtudazo <maevirtudazo6868@gmail.com>
2025-10-18 17:33:57 +0800

    add outputs

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -1,3 +1,24 @@
 output "vpc_id" {
-  value = module.network.vpc_id
+  value = module.network.this.vpc_id
 }
+
+output "security_groups_ids" {
+  value = [module.network.aws_security_group.this.id]
+}
+
+output "subnet_id" {
+  value = module.network.aws_subnet.this.id
+}
+
+output "instance_id" {
+  value = module.compute.this.id
+}
+
+output "public_ip" {
+  value = module.compute.aws_instance.this.public_ip
+}
+
+output "private_ip" {
+  value = module.compute.aws_instance.this.private_ip
+}
+
